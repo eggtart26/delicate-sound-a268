97a0d7524060a3bb6902a4b9484fd0b786ad5db6
justinguanTelus <justin.guan@telus.com>
2025-04-03 22:54:54 -0400

    changed text 3

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + testing deploy 2</h1>
+      <h1>Vite + React + testing deploy 3</h1>
       <div className="card">
         <button
           onClick={() => setCount((count) => count + 1)}
